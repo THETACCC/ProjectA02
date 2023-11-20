88333adb4cb91ee49735c1cd7beba56fb1673842
THETACCC <qingyuanfan03@gmail.com>
2023-11-20 17:11:56 -0500

    inventory update

--- Assets/GameLogic/Inventort System/InventoryManager.cs
+++ Assets/GameLogic/Inventort System/InventoryManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class InventoryManager : MonoBehaviour
+public class InventoryManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     // Start is called before the first frame update
+    GameObject draggedObject;
+    GameObject lastItemSlot;
+    
+
     void Start()
     {
         
@@ -13,6 +19,51 @@ public class InventoryManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (draggedObject != null)
+        {
+            draggedObject.transform.position = Input.mousePosition;
+        }
     }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Left)
+        {
+            GameObject clickedObject = eventData.pointerCurrentRaycast.gameObject;
+            InventorySlots slot = clickedObject.GetComponent<InventorySlots>();
+
+            if (slot != null && slot.heldItem != null)
+            {
+                draggedObject = slot.heldItem;
+                slot.heldItem = null;
+                lastItemSlot = clickedObject;
+            }
+
+            Debug.Log(eventData.pointerCurrentRaycast.gameObject);
+        }
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (draggedObject != null && eventData.pointerCurrentRaycast.gameObject != null && eventData.button == PointerEventData.InputButton.Left)
+        {
+            GameObject clickedobject = eventData.pointerCurrentRaycast.gameObject;
+            InventorySlots slot = clickedobject.GetComponent<InventorySlots>();
+
+            if (slot != null && slot.heldItem == null)
+            {
+                slot.SetHeldItem(draggedObject);
+                draggedObject = null;
+            }
+            else if (slot != null && slot.heldItem != null)
+            {
+                lastItemSlot.GetComponent<InventorySlots>().SetHeldItem(slot.heldItem);
+                slot.SetHeldItem(draggedObject);
+                draggedObject = null;
+            }
+        }
+    }
+    
+
 }
