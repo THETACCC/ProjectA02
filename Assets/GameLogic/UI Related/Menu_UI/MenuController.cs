5a84214468412c95ca41b837bf99c56e019af2ae
Jasminezhang666666 <jz5144@nyu.edu>
2024-10-01 16:30:03 -0400

    World1 UI info弹出，Menu要重写

---
+++ Assets/GameLogic/UI Related/Menu_UI/MenuController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using SKCell;
+
+public class MenuController : MonoBehaviour
+{
+    [Header("Levels To Load")]
+    [SerializeField] private SceneTitle sceneToLoad;  // Pick from available SceneTitles
+    private bool startLoading = false;
+    private FlowManager flowManager;
+    private GameObject flowmanager;
+
+    [SerializeField] private GameObject noSavedGameDialog = null;
+
+    private void Start()
+    {
+        flowmanager = GameObject.Find("FlowManager");
+        flowManager = flowmanager.GetComponent<FlowManager>();
+    }
+
+    public void newGameDialogYes()
+    {
+        // Load the scene with a delay using the SceneTitle
+        LoadNextLevel(sceneToLoad);
+    }
+
+    public void LoadGameDialogYes()
+    {
+        if (PlayerPrefs.HasKey("SavedLevel"))
+        {
+            // Load the saved level with a delay
+            string savedLevel = PlayerPrefs.GetString("SavedLevel");
+            SceneTitle savedSceneTitle = (SceneTitle)System.Enum.Parse(typeof(SceneTitle), savedLevel); // Convert string to SceneTitle
+            LoadNextLevel(savedSceneTitle);
+        }
+        else
+        {
+            noSavedGameDialog.SetActive(true);
+        }
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    private void LoadNextLevel(SceneTitle sceneTitle)
+    {
+        // Use SKUtils.InvokeAction to add a delay before loading the scene
+        SKUtils.InvokeAction(0.2f, () =>
+        {
+            flowManager.LoadScene(new SceneInfo()
+            {
+                index = sceneTitle,  // Load the scene by SceneTitle
+            });
+        });
+        startLoading = true;
+    }
+}
--- Assets/GameLogic/WorldMap/LevelTrigger.cs
+++ Assets/GameLogic/WorldMap/LevelTrigger.cs
@@ -1,71 +1,3 @@
-/*
-using SKCell;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LevelTrigger : MonoBehaviour
-{
-    private bool allowInput = false;
-    private FlowManager flowManager;
-    public SceneTitle scenetitle;
-    public static int t_spawnPoint;
-
-    private GameObject flowmanager;
-
-
-    private bool startloading = false;
-
-    private void Start()
-    {
-
-        flowmanager = GameObject.Find("FlowManager");
-        flowManager = flowmanager.GetComponent<FlowManager>();
-    }
-
-    private void Update()
-    {
-        if(allowInput)
-        {
-            if(Input.GetKeyDown(KeyCode.F))
-            {
-                Debug.Log("INPUT");
-                LoadNextLevel();
-            }
-        }
-    }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Player")
-        {
-            allowInput = true;
-        }
-    }
-
-    public void OnTriggerExit(Collider other)
-    {
-        if(other.gameObject.tag == "Player")
-        {
-            allowInput = false;
-        }
-    }
-    private void LoadNextLevel()
-    {
-
-        SKUtils.InvokeAction(0.2f, () =>
-        {
-
-
-            flowManager.LoadScene(new SceneInfo()
-            {
-                index = scenetitle,
-            });
-        });
-        startloading = true;
-    }
-}
-*/
 using SKCell;
 using System.Collections;
 using UnityEngine;
@@ -85,6 +17,18 @@ public class LevelTrigger : MonoBehaviour
     {
         flowmanager = GameObject.Find("FlowManager");
         flowManager = flowmanager.GetComponent<FlowManager>();
+
+
+
+
+
+        // Print the location of the GameObject with FlowManager script
+        if (flowmanager != null)
+        {
+            Debug.Log("FlowManager's location: " + flowmanager.transform.position);
+        }
+
+
 
         // Ensure the image is hidden at the start
         if (imageMover != null)
