dcc307a5b1555a7dee3cef8d069db4b979dcfbd5
HenkFan <129189902+THETACCC@users.noreply.github.com>
2025-06-06 11:53:20 -0400

    added reward counting mechanics

---
+++ Assets/GameLogic/Level/Level Mechanics/RewardManager.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardManager : MonoBehaviour
+{
+
+    LevelController levelcontroller;
+
+    private bool isRewardSetup = false;
+    // This array will be populated at runtime with all GameObjects tagged "Reward"
+    public GameObject[] RewardObjects;
+
+    // Internal cache: one RewardAcquire per RewardObject
+    private RewardAcquire[] rewardAcquireScripts;
+
+    // Flags to remember if we've already counted a given RewardAcquire.isReached
+    private bool[] rewardReachedFlags;
+
+    // The total number of rewards that have become "isReached == true" so far
+    public int rewardsReachedCount = 0;
+    //public GameObject[] RewardObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject levelcontrollerOBVJ = GameObject.FindGameObjectWithTag("LevelPhaseControll");
+        levelcontroller = levelcontrollerOBVJ.GetComponent<LevelController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Once the level is in the Running phase, perform the one-time search
+        if (levelcontroller != null
+            && levelcontroller.phase == LevelPhase.Running
+            && !isRewardSetup)
+        {
+            SreachRewardObjects();
+            isRewardSetup = true;
+        }
+
+        // After setup is done, continuously check for newly reached rewards
+        if (isRewardSetup)
+        {
+            UpdateRewardCount();
+        }
+    }
+
+    void SreachRewardObjects()
+    {
+        // 1. Find every GameObject tagged "Reward"
+        RewardObjects = GameObject.FindGameObjectsWithTag("Reward");
+
+        // 2. Allocate arrays of the same length
+        int count = RewardObjects.Length;
+        rewardAcquireScripts = new RewardAcquire[count];
+        rewardReachedFlags = new bool[count];
+
+        // 3. For each reward GameObject, get its RewardAcquire component
+        for (int i = 0; i < count; i++)
+        {
+            GameObject go = RewardObjects[i];
+            RewardAcquire acquireScript = go.GetComponent<RewardAcquire>();
+
+            if (acquireScript != null)
+            {
+                rewardAcquireScripts[i] = acquireScript;
+                // Initialize the flag to whatever the current isReached is
+                rewardReachedFlags[i] = acquireScript.isReached;
+            }
+            else
+            {
+                // If any Reward©\tagged object lacks RewardAcquire, log a warning
+                Debug.LogWarning("[RewardManager] GameObject '"
+                    + go.name
+                    + "' has tag 'Reward' but does not contain a RewardAcquire component.");
+                rewardAcquireScripts[i] = null;
+                rewardReachedFlags[i] = false;
+            }
+        }
+
+        Debug.Log("[RewardManager] Found "
+            + count
+            + " reward object(s) in the scene.");
+    }
+
+    void UpdateRewardCount()
+    {
+        if (rewardAcquireScripts == null)
+            return;
+
+        for (int i = 0; i < rewardAcquireScripts.Length; i++)
+        {
+            RewardAcquire script = rewardAcquireScripts[i];
+
+            if (script == null)
+                continue;
+
+            // If this reward has just become "reached" but wasn't before
+            if (script.isReached && !rewardReachedFlags[i])
+            {
+                // 1) Increment the total
+                rewardsReachedCount++;
+
+                // 2) Mark as counted
+                rewardReachedFlags[i] = true;
+
+                // 3) (Optional) Log or fire off an event
+                Debug.Log("[RewardManager] Reward reached on '"
+                    + RewardObjects[i].name
+                    + "'. Total reached: "
+                    + rewardsReachedCount);
+            }
+        }
+    }
+
+}
---
+++ Assets/GameLogic/Level/Level Mechanics/RewardUIManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardUIManager : MonoBehaviour
+{
+    [Header("Reference to the RewardManager that tracks collected rewards")]
+    [Tooltip("If not assigned in the Inspector, this script will attempt to find a RewardManager in the scene at runtime.")]
+    public RewardManager rewardManager;
+
+    [Header("List of UI image GameObjects to toggle on as rewards are collected")]
+    [Tooltip("Order these in the Inspector from first reward ¡ú last reward.")]
+    public GameObject[] rewardImageObjects;
+
+    // Cache the last known count so we only update the UI when it changes
+    private int lastKnownCount = -1;
+
+    void Start()
+    {
+        // If no RewardManager was assigned in the Inspector, try to find one automatically
+        if (rewardManager == null)
+        {
+            rewardManager = FindObjectOfType<RewardManager>();
+            if (rewardManager == null)
+            {
+                Debug.LogError(
+                    "[RewardUIManager] Unable to find a RewardManager in the scene. " +
+                    "Please assign one in the Inspector or ensure a RewardManager exists."
+                );
+            }
+        }
+
+        // Optional: initialize all images as inactive
+        if (rewardImageObjects != null)
+        {
+            for (int i = 0; i < rewardImageObjects.Length; i++)
+            {
+                rewardImageObjects[i].SetActive(false);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (rewardManager == null || rewardImageObjects == null)
+            return;
+
+        int currentCount = rewardManager.rewardsReachedCount;
+
+        // Only update the UI if the count has changed
+        if (currentCount != lastKnownCount)
+        {
+            lastKnownCount = currentCount;
+            RefreshRewardImages(currentCount);
+        }
+    }
+
+    /// <summary>
+    /// Enables the first 'count' images in rewardImageObjects[], disables the others.
+    /// </summary>
+    /// <param name="count">Number of rewards collected (and thus how many images should be active).</param>
+    private void RefreshRewardImages(int count)
+    {
+        int totalImages = rewardImageObjects.Length;
+
+        for (int i = 0; i < totalImages; i++)
+        {
+            // If index is less than the number of collected rewards, set active; otherwise, deactivate
+            bool shouldBeActive = (i < count);
+            if (rewardImageObjects[i].activeSelf != shouldBeActive)
+            {
+                rewardImageObjects[i].SetActive(shouldBeActive);
+            }
+        }
+    }
+}
