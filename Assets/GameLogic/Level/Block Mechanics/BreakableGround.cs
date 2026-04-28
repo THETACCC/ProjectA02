a61727dae1b3671c7e7be44c083a61509fdaf920
THETACCC <qingyuanfan03@gmail.com>
2026-04-27 23:01:50 -0400

    FIxing green and pink block

--- Assets/GameLogic/Level/Block Mechanics/BreakableGround.cs
+++ Assets/GameLogic/Level/Block Mechanics/BreakableGround.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class BreakableGround : MonoBehaviour
 {
     public bool isBreak = false;
-
+    public GameObject myVisualObject;
+    public BoxCollider myCollider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myCollider = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       if( LevelController.instance.phase == LevelPhase.Draging )
+        {
+            myCollider.enabled = false;
+        }
+        else
+        {
+            myCollider.enabled = true;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -37,7 +45,12 @@ public class BreakableGround : MonoBehaviour
 
     private void BreakAndDestroy()
     {
+        if (myVisualObject != null)
+        {
+            myVisualObject.gameObject.SetActive(false);
+        }
         isBreak = true;
+
         //Destroy(gameObject);
     }
 
--- Assets/GameLogic/Level/Block Mechanics/SpawnBlock.cs
+++ Assets/GameLogic/Level/Block Mechanics/SpawnBlock.cs
@@ -6,36 +6,81 @@ public class SpawnBlock : MonoBehaviour
 {
     public GameObject blockToSpawn;
     public bool isSpawn = false;
-    // Start is called before the first frame update
+    public BoxCollider myCollider;
+
+    [Header("Protection")]
+    public float minimumStayTime = 0.2f;
+
+    private bool playerEntered = false;
+    private float enterTime = 0f;
+
     void Start()
     {
-        blockToSpawn = GetComponentInChildren<GameObject>();
+        myCollider = GetComponent<BoxCollider>();
+
+        if (transform.childCount > 0)
+            blockToSpawn = transform.GetChild(0).gameObject;
+
+        if (blockToSpawn != null)
+            blockToSpawn.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
-    
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.tag == "Player2")
+        bool shouldEnable = LevelController.instance.phase != LevelPhase.Draging;
+
+        if (myCollider != null && myCollider.enabled != shouldEnable)
         {
-            StartSpawnBool();
+            // If collider is being disabled, cancel current trigger state
+            if (!shouldEnable)
+            {
+                playerEntered = false;
+            }
+
+            myCollider.enabled = shouldEnable;
         }
     }
-    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isSpawn) return;
+        if (LevelController.instance.phase == LevelPhase.Draging) return;
+
+        if (other.CompareTag("Player2"))
+        {
+            playerEntered = true;
+            enterTime = Time.time;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isSpawn) return;
+        if (!playerEntered) return;
+        if (LevelController.instance.phase == LevelPhase.Draging) return;
+
+        if (other.CompareTag("Player2"))
+        {
+            float stayedLongEnough = Time.time - enterTime;
+
+            if (stayedLongEnough >= minimumStayTime)
+            {
+                StartSpawnBool();
+                StartSpawn();
+            }
+
+            playerEntered = false;
+        }
+    }
+
     private void StartSpawnBool()
     {
-        isSpawn= true;
+        isSpawn = true;
     }
 
-    //This will be referenced from the Block Script
     public void StartSpawn()
     {
-        blockToSpawn.SetActive(true);
+        if (blockToSpawn != null)
+            blockToSpawn.SetActive(true);
     }
-
-
 }
